4dce39587b02f1fdc2f822ac16176afe01978701
caind <caindeanw@gmail.com>
2026-05-27 22:42:30 +0800

    feat: developed manage employees page

---
+++ src/app/CompanyAdmin/manage-employees/manage-employees.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-employees',
+  imports: [],
+  templateUrl: './manage-employees.html',
+  styleUrl: './manage-employees.scss',
+})
+export class ManageEmployees {
+
+}
